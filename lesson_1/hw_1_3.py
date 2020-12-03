629d58d99c48219d30192ef6bafb81ed2cd76519
Alex-Iskar <123!Immortal!123>
2020-12-04 00:07:10 +0300

    [upd] first_home_work

---
+++ lesson_1/hw_1_3.py
@@ -0,0 +1,6 @@
+# Произвести суммирование введенных чисел в формате n + nn + nnn
+n = input("Введи число 'n': ")
+sum_n2 = int(n * 2)
+sum_n3 = int(n * 3)
+sum_n = int(n) + sum_n2 + sum_n3
+print(sum_n)
