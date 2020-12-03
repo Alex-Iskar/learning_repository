55d1527e1c1783d86372f5d43ff2927a6ee19211
Alex-Iskar <123!Immortal!123>
2020-12-03 23:46:53 +0300

    [upd] first_home_work

--- lesson_1/hw_1_2.py
+++ lesson_1/hw_1_2.py
@@ -1,20 +1,23 @@
 # Перевод времени в часы, минуты и секунды и вывод в формате чч:мм:сс
 time = int(input("Введите количество секунд: "))
-if time >= 36000:
-    mm = time / 60
-    hh = mm / 60
+ss = time % 60
+mm = (time / 60) % 60
+hh = (time / 3600)
+if time > 359999:
+    print("Ошибка! Указанное количество секунд превышает ограничения допустимого формата отображения времени!")
+elif hh < 10 and mm < 10 and ss < 10:
+    print("0%d : 0%d : 0%d" % (hh, mm, ss))
+elif hh < 10 and mm < 10 <= ss:
+    print("0%d : 0%d : %d" % (hh, mm % 60, ss % 60))
+elif hh < 10 and mm >= 10 > ss:
+    print("0%d : %d : 0%d" % (hh, mm % 60, time % 60))
+elif hh < 10 <= mm and ss >= 10:
+    print("0%d : %d : %d" % (hh, mm % 60, time % 60))
+elif hh >= 10 > mm and ss < 10:
     print("%d : 0%d : 0%d" % (hh, mm % 60, time % 60))
-elif time < 36000:
-    mm = time / 60
-    hh = mm / 60
-    print("0%d : 0%d : 0%d" % (hh, mm % 60, time % 60))
-elif time < 3600:
-    mm = time / 60
-    print("0%d : %d : 0%d" % (0, mm, time % 60))
-elif time < 600:
-    mm = time / 60
-    print("0%d : 0%d : 0%d" % (0, mm, time % 60))
-elif time < 60:
-    print("0%d : 0%d : %d" % (0, 0, time))
-elif time < 10:
-    print("0%d : 0%d : 0%d" % (0, 0, time))
+elif hh >= 10 > mm and ss >= 10:
+    print("%d : 0%d : %d" % (hh, mm % 60, time % 60))
+elif hh >= 10 and mm >= 10 > ss:
+    print("%d : %d : 0%d" % (hh, mm % 60, time % 60))
+elif hh >= 10 and mm >= 10 and ss >= 10:
+    print("%d : %d : %d" % (hh, mm % 60, time % 60))
