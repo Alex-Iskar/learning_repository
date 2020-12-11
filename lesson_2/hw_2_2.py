21a01d4884137550efe561249df25f056c133f7e
Alex-Iskar <123!Immortal!123>
2020-12-11 09:59:49 +0300

    [upd] second_home_work

--- lesson_2/hw_2_2.py
+++ lesson_2/hw_2_2.py
@@ -4,8 +4,6 @@ list_a = list(input("Введите список значений: "))
 d = len(list_a)-1
 for i in list_a:
     if n < d:
-        a = list_a[n]
-        list_a[n] = list_a[n+1]
-        list_a[n+1] = a
+        list_a[n], list_a[n+1] = list_a[n+1], list_a[n]
     n += 2
 print(list_a)
