5817bc47e3fbb29793682450285ff197bc2b4387
Alex-Iskar <123!Immortal!123>
2020-12-19 19:04:04 +0300

    [upd] five_home_work

--- lesson_5/hw_5_5.py
+++ lesson_5/hw_5_5.py
@@ -1,1 +1,8 @@
 #  Создать (программно) текстовый файл, записать в него программно набор чисел, разделенных пробелами.
+with open("hw_5_5_file.txt", "w") as f_o:
+    new_num = input("Введите числа с разделителем 'пробел' :")
+    f_o.write(new_num)
+with open("hw_5_5_file.txt", "r") as f_o:
+    new_line = f_o.readline()
+    sum_numb_list = map(int, new_line.split())
+    print(sum(sum_numb_list))
